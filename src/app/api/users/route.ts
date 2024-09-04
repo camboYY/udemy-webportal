9611e80a8da67f448b8bbf8d2da6eec9daa0eb27
Yous Yoeun <yous.yoeun@rhbgroup.com.kh>
2024-09-04 21:54:39 +0700

    push our app

--- src/app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request, res: Response) {
-  const { name, email, password } = await req.json();
-  console.log({ req, res, name, email, password });
-
-  return NextResponse.json({ success: "Hello world" });
-}
